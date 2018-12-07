9c4b5f22364945d72afadf6e5b748739a90a4277
Veniel Dorestave <veniel@noukod.com>
2018-12-07 15:01:47 +0000

    Done.

--- app/item.rb
+++ app/item.rb
@@ -1,8 +1,13 @@
 class Item
   attr_accessor :name, :price
-
+  @@items = []
   def initialize(name,price)
     @name = name
     @price = price
+    @@items << self
+  end
+
+  def self.all
+    @@items
   end
 end
