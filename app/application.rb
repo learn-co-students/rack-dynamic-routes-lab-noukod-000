e8f3ff73645600f26edd1f587455f15b3a17fe6e
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-05-22 18:45:01 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@
 #
 #   def call(env)
 #     resp = Rack::Response.new
-#     req = Rack::Response.new(env)
+#     req = Rack::Request.new(env)
 #
 #     if req.path.match(/items/)
 #       item_name = req.path.split(/items/).last
